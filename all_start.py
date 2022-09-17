05d8262f21860cad4d41e8972cf73d7431bb40d4
Prashant Jadon <67583008+prashant-jadon@users.noreply.github.com>
2022-09-17 18:44:55 +0530

    Update all_start.py

--- all_start.py
+++ all_start.py
@@ -1,5 +1,6 @@
 import os
 os.system("python3 req_down.py| tee -a files_to_download.txt")
 os.system("python3 json_cov.py")
-os.system("python3 names.py | tee -a names_new.html")
+os.system("python3 names.py >> names_new.html")
+os.system("python3 -m http.server")
 
